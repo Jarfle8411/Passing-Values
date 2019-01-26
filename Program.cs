01264ca58f8a04b5783e5d803fdb59a65ba708c0
Jarvis <39226019+Jarfle8411@users.noreply.github.com>
2019-01-26 15:51:40 -0500

    Add files via upload
    I have created a program which passes values to an method called FeettoInches where the method does the calculations, and returns the value to the calling method FeetToInches.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PassingInformatiom
+{
+    class Page_3_12_by_30_Calculation_Function
+    {
+        static void Main(string[] args)
+        {
+            //Simply passing information to method Feet_To_Inches
+            Console.Write("\n{0}",FeetToInches(30));
+            Console.Write("\n{0}\n", FeetToInches(100));
+        }
+
+        // Recieving a call from the calling method
+        // and this method is returing a call
+        public static int FeetToInches(int x)
+        {
+            //declaration
+            int feet, recieved, calculation;
+            //initializing the decalaration
+            feet = 12;
+            recieved = x;
+            //Calculation of the variables
+            calculation = feet * recieved;
+            // Returning the calculation to the calling method
+            return calculation;
+        }
+    }
+}
